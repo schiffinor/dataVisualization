62b2ef241b5cdc46bd60f2a95f3a1db40d2cef3a
schiffinor <128438356+schiffinor@users.noreply.github.com>
2024-02-29 06:11:55 -0500

    Fully Full P1

--- Project1/main.py
+++ Project1/main.py
@@ -2,15 +2,22 @@
 
 """
 
-from data import Data
+import numpy as np
 
 
 def main():
-    iris_data = Data('data/iris_no_species.csv')
-    print(iris_data)
-    print(iris_data.head())
-    print(iris_data.tail())
-
+    listo = []
+    ref_list = [[[0, 1, 2, 3, 4, 5, 6, 7, 8, 9],[0, 1, 2, 3, 4, 5, 6, 7, 8, 9],[0, 1, 2, 3, 4, 5, 6, 7, 8, 9],[0, 1, 2, 3, 4, 5, 6, 7, 8, 1]]]
+    for i in ref_list:
+        for j in i:
+            vec = j.copy()
+            vec.sort()
+            listo.append(vec)
+    print(f'listo: {listo}')
+    all_faces = np.array(listo)
+    print(f'all_faces: {all_faces}')
+    unique_faces = np.unique(all_faces, axis=0)
+    print(f'unique_faces: {unique_faces}')
 
 if __name__ == '__main__':
     main()
